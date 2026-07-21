f243a99b847802f2aa14405390f27a0f04989eb7
abohssan555-dot <abohssan555@gmail.com>
2026-07-21 23:37:17 +0300

    Refactor main.jsx to include React imports and dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ManagerDashboard from "./ManagerDashboard.jsx";
+import "./index.css";
 
-html, body, #root {
-  height: 100%;
-  margin: 0;
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ManagerDashboard />
+  </React.StrictMode>
+);
