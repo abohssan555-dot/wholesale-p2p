5d58e292789b49f65de91e742759c22bd34942f3
abohssan555-dot <abohssan555@gmail.com>
2026-07-22 15:30:10 +0300

    Add routing for ManagerDashboard and TraderOnboarding

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ManagerDashboard from "./ManagerDashboard.jsx";
+import TraderOnboarding from "./TraderOnboarding.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ManagerDashboard />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ManagerDashboard />} />
+        <Route path="/trader" element={<TraderOnboarding />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
