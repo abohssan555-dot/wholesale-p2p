e941c7279ef3d2b280ac9e8ca53f254955304e70
abohssan555-dot <abohssan555@gmail.com>
2026-07-22 15:43:16 +0300

    Implement redirect from home to admin dashboard
    Added a redirect from the home page to the admin dashboard.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ManagerDashboard from "./ManagerDashboard.jsx";
 import TraderOnboarding from "./TraderOnboarding.jsx";
 import "./index.css";
@@ -9,6 +9,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        {/* مؤقت: يحول الصفحة الرئيسية لدخول المدير لين نبني صفحة هبوط حقيقية */}
+        <Route path="/" element={<Navigate to="/admin" replace />} />
         <Route path="/admin" element={<ManagerDashboard />} />
         <Route path="/trader" element={<TraderOnboarding />} />
       </Routes>
